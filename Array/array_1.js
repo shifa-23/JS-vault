f7f584d29effa5a6cd313c35f377fb1d241c4eac
Shifa <shifaparveen0310@gmail.com>
2025-04-09 20:39:19 +0530

    Add files via upload

---
+++ Array/array_1.js
@@ -0,0 +1,60 @@
+// /////array
+// const myArray = [0,1,2,3,4,5,true, "shifa"]
+// console.log(myArray);
+// console.log(myArray[3])
+
+
+// console.log(myArray[5]);
+// console.log(myheros[3]);
+
+// //methods
+
+// const array2= new Array(1,2,3,4,5)
+// const myheros = ['shaktiman','nagraj']
+
+//whn we open console we get
+// (4) [1, 2, 3, 4]0: 11: 22: 33: 4length: 4[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Objectconstructor: ƒ Object()hasOwnProperty: ƒ hasOwnProperty()isPrototypeOf: ƒ isPrototypeOf()propertyIsEnumerable: ƒ propertyIsEnumerable()toLocaleString: ƒ toLocaleString()toString: ƒ toString()valueOf: ƒ valueOf()__defineGetter__: ƒ __defineGetter__()__defineSetter__: ƒ __defineSetter__()__lookupGetter__: ƒ __lookupGetter__()__lookupSetter__: ƒ __lookupSetter__()__proto__: (...)get __proto__: ƒ __proto__()set __proto__: ƒ __proto__()
+
+//look here we have 2 procedure
+//1st ->after 1st procedure we get secondsa
+//2nd ->
+
+
+//******************************************ARRAY - METHOD******************************************** */
+const myArray = [0,1,2,3,4,5]
+myArray.push(9)
+console.log(myArray);
+myArray.push("shifa")
+console.log(myArray);
+
+myArray.unshift(9)
+myArray.shift() 
+myArray.shift()
+myArray.shift()
+myArray.shift()
+myArray.shift()
+myArray.pop()
+myArray.shift()
+
+console.log(myArray.includes(9))
+console.log(myArray.indexOf(9))
+
+//here to add the elemnt in the first place
+console.log(myArray);
+const myArray2 = myArray.join()
+console.log(myArray)
+console.log(myArray2)
+console.log(typeof myArray2);
+console.log(typeof myArray);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Array/slice.js
@@ -0,0 +1,15 @@
+//***********************SET 1************************************* */
+const myArray = [0,1,2,3,4,5]
+console.log("A", myArray)
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+
+//***********************SET 2************************************* */
+
+console.log("B", myArray)
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+
+
+//***********************SET 3************************************* */
+console.log("C", myArray)
